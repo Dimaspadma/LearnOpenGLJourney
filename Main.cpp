4292ceb1c1e171d71a70f81a95339460418a62f9
Dimaspadma <dimaspadma@gmail.com>
2022-10-14 01:59:26 +0700

    Add Vertex Shader and Fragment Shader

--- Main.cpp
+++ Main.cpp
@@ -36,6 +36,57 @@ int main() {
 		return -1;
 	}
 
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0,
+		 0.5f, -0.5f, 0.0,
+		 0.0,   0.5f, 0.0
+	};
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	const char* vertexShaderSource = R"(#version 330 core 
+layout (location = 0) in vec3 aPos;
+void main()
+{
+	gl_Position = vec4(aPos, 1.0);
+})";
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		LOG("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog);
+	}
+
+	const char* fragmentShaderSource = R"(#version 330 core 
+out vec4 FragColor;
+void main()
+{
+	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+})";
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success) {
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		LOG("ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog);
+	}
+
 	// render loop
 	while(!glfwWindowShouldClose(window)) {
 
